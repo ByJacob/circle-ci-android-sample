d268d5e0918b5e600115fa7f1607417d9895f3d0
shoheiskawano <shoheiskawano@gmail.com>
2015-06-04 09:48:04 +0900

    Initialize

---
+++ app/src/androidTest/java/com/shoheiskawano/circlecisample/HelloCircleCiTest.java
@@ -0,0 +1,26 @@
+package com.shoheiskawano.circlecisample;
+
+import android.test.ActivityInstrumentationTestCase2;
+import android.widget.TextView;
+
+/**
+ * <a href="http://d.android.com/tools/testing/testing_android.html">Testing Fundamentals</a>
+ */
+public class HelloCircleCiTest extends ActivityInstrumentationTestCase2<MainActivity> {
+
+    TextView mTextView;
+
+    public HelloCircleCiTest() {
+        super(MainActivity.class);
+    }
+
+    @Override
+    protected void setUp() throws Exception {
+        super.setUp();
+        mTextView = (TextView) getActivity().findViewById(R.id.activity_main_text_hello);
+    }
+
+    public void testHelloCircleCiString() {
+        assertEquals(mTextView.getText().toString(), "fail");
+    }
+}
